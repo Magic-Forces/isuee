4f5d64ab67f86369eaf4a166e0ea5028b14e79d4
Magic-Forces <root@foxlab.pl>
2025-11-12 21:43:37 +0100

    feat(srednia): add arithmetic and geometric mean calculations

---
+++ c/srednia_geometryczna/srednia_geometryczna.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double a, b, c;
+
+    do
+    {
+        printf("Podaj 1 liczbe: ");
+        fflush(stdin);
+    } while (!scanf("%lf", &a));
+
+    do
+    {
+        printf("Podaj 2 liczbe: ");
+        fflush(stdin);
+    } while (!scanf("%lf", &b));
+
+    do
+    {
+        printf("Podaj 3 liczbe: ");
+        fflush(stdin);
+    } while (!scanf("%lf", &c));
+
+    printf("Srednia geometryczna: %lf", cbrt(a * b * c));
+
+    return 0;
+}
