73c23129358ce7e88b926918e5f5997a8d98e252
sm0qs <74928880+sm0qs@users.noreply.github.com>
2025-11-12 21:43:37 +0100

    feat(srednia): add arithmetic and geometric mean calculations

---
+++ c/srednia_arytmetyczna/srednia_arytmetyczna.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    double a, b, c;
+
+    do
+    {
+        printf("Podaj 1 liczbe: ");
+        fflush(stdin);
+    } while (!scanf("%lf", &a));
+
+    do
+    {
+        printf("Podaj 2 liczbe: ");
+        fflush(stdin);
+    } while (!scanf("%lf", &b));
+
+    do
+    {
+        printf("Podaj 3 liczbe: ");
+        fflush(stdin);
+    } while (!scanf("%lf", &c));
+
+    printf("Srednia arytmetyczna: %lf", (a + b + c) / 3.0);
+
+    return 0;
+}
