090df40bd56d8b2e81346b66c5aec7182e989ae1
sm0qs <74928880+sm0qs@users.noreply.github.com>
2025-11-19 20:51:38 +0100

    refactor(rownanie_kwadratowe): streamline input prompts and improve variable scope

--- c/rownanie_kwadratowe/rownanie_kwadratowe.c
+++ c/rownanie_kwadratowe/rownanie_kwadratowe.c
@@ -1,69 +1,77 @@
 #include <stdio.h>
 #include <math.h>
-
-double a, b, c;
+#include <ctype.h>
 
 int main()
 {
+    double a, b, c;
+
+    printf("Program wyznacza rozwiazania rownania: ax + bx + c = 0\n");
 
     do
     {
-        printf("Podaj wspolczynnik a: ");
-        fflush(stdin);
-    } while (!scanf("%lf", &a));
-
-    do
-    {
-        printf("Podaj wspolczynnik b: ");
-        fflush(stdin);
-    } while (!scanf("%lf", &b));
-
-    do
-    {
-        printf("Podaj wspolczynnik c: ");
-        fflush(stdin);
-    } while (!scanf("%lf", &c));
-
-    // ax^2 + bx + c = 0
-
-    if (a == 0)
-    {
-        printf("Rówanie jest liniowe");
-        // bx+c=0 | -c -> bx=-c |:b -> x=-c/b
-        if (b == 0)
+        do
         {
-            if (c == 0)
+            printf("Podaj wspolczynnik a: ");
+            fflush(stdin);
+        } while (!scanf("%lf", &a));
+
+        do
+        {
+            printf("Podaj wspolczynnik b: ");
+            fflush(stdin);
+        } while (!scanf("%lf", &b));
+
+        do
+        {
+            printf("Podaj wspolczynnik c: ");
+            fflush(stdin);
+        } while (!scanf("%lf", &c));
+
+        // ax^2 + bx + c = 0
+
+        if (a == 0)
+        {
+            printf("Rówanie jest liniowe");
+            // bx+c=0 | -c -> bx=-c |:b -> x=-c/b
+            if (b == 0)
             {
-                printf("Nieszkonczenie wiele rozwiazan");
+                if (c == 0)
+                {
+                    printf("Nieszkonczenie wiele rozwiazan");
+                }
+                else
+                {
+                    printf("Rownanie sprzeczne");
+                }
             }
             else
             {
-                printf("Rownanie sprzeczne");
+                printf("x =  %lf", -c / b);
             }
         }
         else
         {
-            printf("x =  %lf", -c / b);
-        }
-    }
-    else
-    {
-        double delta = pow(b, 2) - (4 * a * c);
+            double delta = b * b - 4 * a * c;
 
-        if (delta < 0)
-        {
-            printf("Brak rozwiazan");
+            if (delta < 0)
+            {
+                printf("Brak rozwiazan");
+            }
+            else if (delta == 0)
+            {
+                printf("x(0) = %lf", -b / (2 * a));
+            }
+            else
+            {
+                printf("x(1) = %lf\n", (-b - sqrt(delta)) / (2 * a));
+                printf("x(2) = %lf\n", (-b + sqrt(delta)) / (2 * a));
+            }
         }
-        else if (delta == 0)
-        {
-            printf("x = %lf", -b / (2 * a));
-        }
-        else
-        {
-            printf("x(1) = %lf\n", (-b - sqrt(delta)) / (2 * a));
-            printf("x(2) = %lf\n", (-b + sqrt(delta)) / (2 * a));
-        }
-    }
+
+        printf("Czy chcesz powtorzyc program? (t - tak, inny znak - nie): ");
+        fflush(stdin);
+    } while (tolower(getchar()) == 't');
 
     return 0;
 }
