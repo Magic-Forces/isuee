44bc94eae8edfa6587600730bd51cb78ab2090a0
sm0qs <74928880+sm0qs@users.noreply.github.com>
2025-11-20 17:03:37 +0100

    refactor(kalkulator): improve variable scope and streamline input handling

--- c/kalkulator/kalkulator.c
+++ c/kalkulator/kalkulator.c
@@ -1,49 +1,99 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-char znak;
-double l1, l2, wynik;
+int enterNumber(double *pliczba);
+double suma(double x, double y);
+double roznica(double x, double y);
+double iloczyn(double x, double y);
+int iloraz(double x, double y, double *pwynik);
 
 int main()
 {
+    char znak;
+    double a, b, wynik;
+    bool stan;
     do
     {
-        printf("Podaj dzialanie (+, -, *, /): \n");
-        fflush(stdin);
-        znak = getchar();
-    } while ((znak != '+') && (znak != '-') && (znak != '*') && (znak != '/'));
-
-    do
-    {
-        printf("Podaj pierwsza liczbe: ");
-        fflush(stdin);
-    } while (!scanf("%lf", &l1));
-
-    do
-    {
-        printf("Podaj druga liczbe: ");
-        fflush(stdin);
-    } while (!scanf("%lf", &l2));
-
-    switch (znak)
-    {
-    case '+':
-        wynik = l1 + l2;
-        break;
-    case '-':
-        wynik = l1 - l2;
-        break;
-    case '*':
-        wynik = l1 * l2;
-        break;
-    case '/':
-        if (l2 == 0)
+        do
         {
-            printf("Nie dziel przez 0");
-            return 0;
+            printf("Podaj dzialanie (+, -, *, /): \n");
+            fflush(stdin);
+            znak = getchar();
+        } while ((znak != '+') && (znak != '-') && (znak != '*') && (znak != '/'));
+
+        do
+        {
+            stan = enterNumber(&a);
+        } while (stan == 0);
+
+        do
+        {
+            stan = enterNumber(&b);
+        } while (stan == 0);
+
+        switch (znak)
+        {
+        case '+':
+            wynik = suma(a, b);
+            break;
+        case '-':
+            wynik = roznica(a, b);
+            break;
+        case '*':
+            wynik = iloczyn(a, b);
+            break;
+        case '/':
+            stan = iloraz(a, b, &wynik);
+            break;
         }
-        wynik = l1 / l2;
-        break;
-    }
-    printf("%lf\n", wynik);
+
+        if (stan == 1)
+        {
+            printf("%lf\n", wynik);
+        }
+        else
+        {
+            printf("Nie dziel przez 0\n");
+        }
+
+        printf("Czy chcesz powtorzyc program? (t - tak, inny znak - nie): ");
+        fflush(stdin);
+    } while (tolower(getchar()) == 't');
     return 0;
+}
+
+int enterNumber(double *pliczba)
+{
+    printf("Podaj liczbe: ");
+    fflush(stdin);
+    return scanf("%lf", pliczba);
+}
+
+double suma(double x, double y)
+{
+    return (x + y);
+}
+
+double roznica(double x, double y)
+{
+    return (x - y);
+}
+
+double iloczyn(double x, double y)
+{
+    return (x * y);
+}
+
+int iloraz(double x, double y, double *pwynik)
+{
+    if (y != 0)
+    {
+        *pwynik = x / y;
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
